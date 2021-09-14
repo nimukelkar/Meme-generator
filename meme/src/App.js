7a7e71dbf4746a7add971b754480e0cc03304025
nimu09 <nimu.kelkar@gmail.com>
2021-09-14 17:59:42 +0530

    Header and Meme Generator added

--- meme/src/App.js
+++ meme/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Header from "./Header"
+import MemeGenerator from "./MemeGenerator"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header/>
+      <MemeGenerator/>
     </div>
   );
 }
---
+++ meme/src/MemeGenerator.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { render } from "react-dom"
+
+class MemeGenerator extends React.Component {
+    constructor(){
+        super()
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <h1>Meme Generator</h1>
+        )
+    }
+}
+
+
+
+
+export default MemeGenerator
