d8c60228c1629eb678346601df593b27067a35fe
nimu09 <nimu.kelkar@gmail.com>
2021-09-14 19:19:18 +0530

    API added

--- meme/src/MemeGenerator.js
+++ meme/src/MemeGenerator.js
@@ -5,8 +5,22 @@ class MemeGenerator extends React.Component {
     constructor(){
         super()
         this.state={
+            topText:"",
+            bottomText:"",
+            randomImg:"http://i.imgflip.com/1bij.jpg",
+            allMemeImgs:[]
 
         }
+    }
+    componentDidMount(){   // Made an API call to get meme images.
+        fetch("https://api.imgflip.com/get_memes")
+        .then(response => response.json())
+        .then(response=>{
+            const{memes}=response.data
+            console.log(memes[0])
+
+            this.setState({allMemeImgs:memes})
+        })
     }
     render(){
         return(
