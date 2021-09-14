4586460c7a6c8bf5ad80871e05c070171dcdc731
nimu09 <nimu.kelkar@gmail.com>
2021-09-14 18:11:21 +0530

    Modified header

--- meme/src/Header.js
+++ meme/src/Header.js
@@ -4,6 +4,7 @@ function Header(){
     return(
        <header>
            <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"/>
+           <p>Meme Generator</p>
        </header>
     )
 }
